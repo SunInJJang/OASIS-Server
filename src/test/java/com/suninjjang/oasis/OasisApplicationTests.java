a13aefde106db6df855bf84e3853f8f046e59888
shgur7236 <shgurtns7236@naver.com>
2022-10-04 22:40:46 +0900

    first commit

---
+++ src/test/java/com/suninjjang/oasis/OasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suninjjang.oasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
