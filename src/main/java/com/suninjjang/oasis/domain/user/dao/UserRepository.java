10506773853b6c337e867708c6a537fca9b569b3
shgur7236 <shgurtns7236@naver.com>
2022-10-28 15:20:01 +0900

    Option 사용

--- src/main/java/com/suninjjang/oasis/domain/user/dao/UserRepository.java
+++ src/main/java/com/suninjjang/oasis/domain/user/dao/UserRepository.java
@@ -4,8 +4,9 @@ import com.suninjjang.oasis.domain.user.domain.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User,Long> {
-    public User findByUsername(String username);
-    public List<User> findAllByUsername(String username);
+public interface UserRepository extends MongoRepository<User,String> {
+     Optional<User> findUserById(String id);
+    List<User> findAllByUsername(String username);
 }
