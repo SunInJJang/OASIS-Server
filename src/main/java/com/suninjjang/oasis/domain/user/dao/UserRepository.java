6dab2a3fa579158eca29ae352725c06a014dd9c3
shgur7236 <shgurtns7236@naver.com>
2022-10-28 14:37:35 +0900

    Long이 아니라 String

--- src/main/java/com/suninjjang/oasis/domain/user/dao/UserRepository.java
+++ src/main/java/com/suninjjang/oasis/domain/user/dao/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface UserRepository extends MongoRepository<User,Long> {
+public interface UserRepository extends MongoRepository<User,String> {
     public User findByUsername(String username);
     public List<User> findAllByUsername(String username);
 }
