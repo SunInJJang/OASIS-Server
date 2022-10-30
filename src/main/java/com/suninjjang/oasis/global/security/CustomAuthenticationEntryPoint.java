54233601668a8261a2b7a07666c434ccde8cd597
shgur7236 <shgurtns7236@naver.com>
2022-10-30 17:59:34 +0900

    CustomAuthenticationEntryPoint 구현

---
+++ src/main/java/com/suninjjang/oasis/global/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.suninjjang.oasis.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.suninjjang.oasis.global.error.ErrorCode;
+import com.suninjjang.oasis.global.error.ErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+        ErrorCode errorCode =  ErrorCode.UNAUTHORIZED;
+        String errorResponseJson = objectMapper.writeValueAsString(
+                new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()));
+
+        response.setStatus(errorCode.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(errorResponseJson);
+    }
+}
