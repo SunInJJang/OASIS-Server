7e0ef931c2b279912c60980393b28178796129a7
shgur7236 <shgurtns7236@naver.com>
2022-11-08 23:38:02 +0900

    LogoutService 구현

---
+++ src/main/java/com/suninjjang/oasis/domain/auth/services/LogoutService.java
@@ -0,0 +1,5 @@
+package com.suninjjang.oasis.domain.auth.services;
+
+public interface LogoutService {
+    void logout();
+}
---
+++ src/main/java/com/suninjjang/oasis/domain/auth/services/impl/LogoutServiceImpl.java
@@ -0,0 +1,19 @@
+package com.suninjjang.oasis.domain.auth.services.impl;
+
+import com.suninjjang.oasis.domain.auth.services.LogoutService;
+import com.suninjjang.oasis.global.util.UserUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class LogoutServiceImpl implements LogoutService {
+    private final UserUtils userUtils;
+
+    @Override
+    public void logout() {
+        userUtils.getCurrentUser().updateRefreshToken(null);
+    }
+}
