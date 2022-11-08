de4b0c0ba4995748be0477e94a4fb330121f0dea
shgur7236 <shgurtns7236@naver.com>
2022-11-08 23:33:04 +0900

    refreshToken과 User 생성자 추가

--- src/main/java/com/suninjjang/oasis/domain/user/domain/entity/User.java
+++ src/main/java/com/suninjjang/oasis/domain/user/domain/entity/User.java
@@ -1,5 +1,6 @@
 package com.suninjjang.oasis.domain.user.domain.entity;
 
+import com.mongodb.lang.Nullable;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,11 +17,25 @@ public class User {
     @Id
     private String id;
 
+    @Nullable()
     private String username;
 
     private String email;
 
     private String password;
 
+    @Nullable()
     private String datedDate;
+
+    private String refreshToken;
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public User(String id, String refreshToken){
+        this.id = id;
+        this.refreshToken = refreshToken;
+    }
+
 }
