679d670599b079374742fe673a193ba3182d8da4
shgur7236 <shgurtns7236@naver.com>
2022-11-08 23:33:26 +0900

    userId로 존재하는지 확인

--- src/main/java/com/suninjjang/oasis/domain/user/dao/UserRepository.java
+++ src/main/java/com/suninjjang/oasis/domain/user/dao/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User,String> {
      Optional<User> findUserById(String id);
     List<User> findAllByUsername(String username);
+
+    boolean existsUserById(String id);
 }
