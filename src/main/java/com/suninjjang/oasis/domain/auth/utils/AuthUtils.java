2b69314d9fcac082fb516bc34ef84d3129ddc7ca
shgur7236 <shgurtns7236@naver.com>
2022-11-08 23:34:43 +0900

    음...하는게 좋은지 모르겠음 다시 해볼 예정

--- src/main/java/com/suninjjang/oasis/domain/auth/utils/AuthUtils.java
+++ src/main/java/com/suninjjang/oasis/domain/auth/utils/AuthUtils.java
@@ -1,7 +1,10 @@
 package com.suninjjang.oasis.domain.auth.utils;
 
 import com.suninjjang.oasis.domain.auth.dto.TokenDto;
+import com.suninjjang.oasis.domain.auth.dto.request.LoginRequest;
 
 public interface AuthUtils {
     TokenDto generateTokenResponse(String email);
+
+    LoginRequest generateLoginResponse(String id, String password);
 }
