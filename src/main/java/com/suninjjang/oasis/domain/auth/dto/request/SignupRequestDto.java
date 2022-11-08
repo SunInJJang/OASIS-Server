47818f7cb4295236b403e08df0cb6c6c14d3c3fb
shgur7236 <shgurtns7236@naver.com>
2022-11-08 09:39:31 +0900

    회원가입요청dto생성

---
+++ src/main/java/com/suninjjang/oasis/domain/auth/dto/request/SignupRequestDto.java
@@ -0,0 +1,15 @@
+package com.suninjjang.oasis.domain.auth.dto.request;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class SignupRequestDto {
+    private final String userId;
+    private final String password;
+    private final String email;
+    private final String nickname;
+    private final String datedDate;
+}
